2150f5d535f570e7f0d8a7de00502ee60d65b193
Andrey <Andrey759@list.ru>
2015-07-13 17:45:26 +0300

    temp

--- Bingo/src/main/java/ru/bingo/controller/MainController.java
+++ Bingo/src/main/java/ru/bingo/controller/MainController.java
@@ -1,17 +1,20 @@
 package ru.bingo.controller;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+
+@RestController
 public class MainController {
-    //Logger log = Logger.getLogger(MainController.class);
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String index() {
-        return "index2.jsp";
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public String index() throws IOException {
+        return "Bingooo!!!";
     }
 
 }
